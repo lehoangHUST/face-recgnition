dcc7789b86f2ec58d589071a2da48f71e2dd1c68
lehoangHUST <lehoangEDABK@gmail.com>
2022-02-24 17:45:32 +0700

    Update

---
+++ utils/augmentation.py
@@ -0,0 +1,113 @@
+import albumentations as A
+import yaml
+import os
+
+class Augmentation:
+    # Init attributes in class 
+    def __init__(self, config):
+        if isinstance(config, dict):
+            config = config
+        elif os.path.isfile(config):
+            with open(config) as f:
+                config = yaml.safe_load(f)
+                print(config)
+        else:
+            raise TypeError
+        self.config = config
+    
+    # Augmentation
+    def define_aug(self):
+        # Recursively compose transform
+        self.augs = {}
+        if self.config["albumentation"]["blur"]["prob"] > 0:
+            self.blur = A.Blur(
+                blur_limit=self.config["albumentation"]["blur"]["strength"],
+                p=self.config["albumentation"]["blur"]["prob"]
+            )
+            self.augs.update({
+                "blur": self.blur
+            })
+        if self.config["albumentation"]["clahe"]["prob"] > 0:
+            self.CLAHE = A.CLAHE(
+                clip_limit=self.config["albumentation"]["clahe"]["clip_limit"],
+                tile_grid_size=self.config["albumentation"]["clahe"]["tile_grid_size"],
+                p=self.config["albumentation"]["clahe"]["prob"]
+            )
+            self.augs.update({
+                "clahe": self.CLAHE
+            })
+        
+        # Color jitter
+        if self.config["albumentation"]["color_jitter"]["prob"] > 0:
+            self.color_jitter = A.ColorJitter(
+                brightness=self.config["albumentation"]["color_jitter"]["brightness"],
+                contrast=self.config["albumentation"]["color_jitter"]["contrast"],
+                saturation=self.config["albumentation"]["color_jitter"]["saturation"],
+                hue=self.config["albumentation"]["color_jitter"]["hue"],
+                p=self.config["albumentation"]["color_jitter"]["prob"]
+            )
+            self.augs.update({
+                "color_jitter": self.color_jitter
+            })
+
+        # Dropout
+        if self.config["albumentation"]["coarse_dropout"]["prob"] > 0:
+            self.cutout = A.CoarseDropout(
+                max_holes=self.config["albumentation"]["coarse_dropout"]["max_holes"],
+                max_height=self.config["albumentation"]["coarse_dropout"]["max_height"],
+                max_width=self.config["albumentation"]["coarse_dropout"]["max_width"],
+                min_holes=self.config["albumentation"]["coarse_dropout"]["min_holes"],
+                min_height=self.config["albumentation"]["coarse_dropout"]["min_height"],
+                min_width=self.config["albumentation"]["coarse_dropout"]["min_width"],
+                fill_value=self.config["albumentation"]["coarse_dropout"]["fill_value"],
+                p=self.config["albumentation"]["coarse_dropout"]["prob"]
+            )
+            self.augs.update({
+                "cutout": self.cutout
+            })
+        if self.config["albumentation"]["downscale"]["prob"] > 0:
+            self.downscale = A.Downscale(
+                scale_min=self.config["albumentation"]["downscale"]["scale_min"],
+                scale_max=self.config["albumentation"]["downscale"]["scale_max"],
+                p=self.config["albumentation"]["downscale"]["prob"]
+            )
+            self.augs.update({
+                "downscale": self.downscale
+            })
+        
+        # Affine transform
+        if self.config["albumentation"]["affine"]["prob"] > 0:
+            self.affine = A.Affine(
+                scale=self.config["albumentation"]["affine"]["scale"],
+                translate_percent=self.config["albumentation"]["affine"]["translate_percent"],
+                rotate=self.config["albumentation"]["affine"]["rotate"],
+                shear=self.config["albumentation"]["affine"]["shear"],
+                fit_output=self.config["albumentation"]["affine"]["fit_output"],
+                p=self.config["albumentation"]["affine"]["prob"]
+            )
+            self.augs.update({
+                "affine": self.affine
+            })
+        
+
+        if self.config["albumentation"]["fliplr"]["prob"] > 0:
+            self.fliplr = A.HorizontalFlip(
+                p=self.config["albumentation"]["fliplr"]["prob"]
+            )
+            self.augs.update({
+                "fliplr": self.fliplr
+            })
+
+        list_transforms = [self.augs[transform] for transform in self.augs.keys()]
+        self.transforms = A.Compose(list_transforms)
+
+    def __call__(self, image):
+        if self.augs:
+            transformed = self.transforms(image=image)
+            image = transformed["image"]
+        return image
+
+# Test
+if __name__ == '__main__':
+    path_file = 'D:/Machine_Learning/Face_Recognition/config/augment.yaml'
+    aug = Augmentation(path_file)
---
+++ utils/dataset.py
@@ -0,0 +1,64 @@
+import os, sys
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+import yaml
+import cv2
+
+from pathlib import Path 
+from tqdm import tqdm
+from sklearn.model_selection import train_test_split
+
+IMG_FORMATS = ['jpg', 'jpeg', 'png', 'bmp']
+
+# FACE RECOGNITION
+"""
+    Path structure for training:
+    - object1/
+        + image 1 of object1
+        + image 2 of object1
+        + .....
+    - object2/
+        + image 1 of object2
+        + image 2 of object2
+        + .....
+    - .......    
+"""
+
+
+class FaceRecognitionDataset:
+    # Init
+    def __init__(self, path: str, ratio: float):
+        list_img = []
+        list_label = []
+        self.ratio = ratio
+        # Check conditional ratio
+        if self.ratio >= 1.0 or self.ratio <= 0.0:
+            raise ValueError
+        self.classes = len(os.listdir(path))
+        if os.path.isdir(path):
+            for obj, pth in enumerate(os.listdir(path)):
+                folder_sub = os.path.join(path, pth)
+                for folder in tqdm(os.listdir(folder_sub), desc=f'{pth}'):
+                    img = cv2.imread(os.path.join(folder_sub, folder))
+                    # Convert BGR to RGB
+                    img = img[:, :, ::-1]
+                    img = cv2.resize(img, (224, 224))
+                    list_img.append(img)
+                list_label.append([obj + 1]*len(os.listdir(pth)))
+            self.features = np.array(list_img)
+            self.label = np.array(list_label).reshape((-1, 1))
+            # train test split
+            self.Xtrain, self.Xtest, self.Ytrain, self.Ytest = train_test_split(self.features, self.label, self.ratio)
+        else:
+            raise TypeError
+
+    # Get dataset
+    def get_data(self):
+        return self.Xtrain, self.Xtest, self.Ytrain, self.Ytest
+
+
+if __name__ == '__main__':
+    path = 'D:/Machine_Learning/Face_Recognition/105_classes_pins_dataset'
+    dataset = FaceRecognitionDataset(path)
