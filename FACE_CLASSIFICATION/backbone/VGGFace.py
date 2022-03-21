81e2892195b08812990aab43832cb90fb6fe9ab7
lehoangHUST <lehoangEDABK@gmail.com>
2022-03-21 22:20:11 +0700

    Train svm, softmax

--- FACE_CLASSIFICATION/backbone/VGGFace.py
+++ FACE_CLASSIFICATION/backbone/VGGFace.py
@@ -49,7 +49,7 @@ def baseModel():
 
 
 # url = 'https://drive.google.com/uc?id=1CPSeum3HpopfomUEK1gybeuIVoeJT_Eo'
-def loadModel(url='https://github.com/serengil/deepface_models/releases/download/v1.0/vgg_face_weights.h5'):
+def VGGFaceModel(url='https://github.com/serengil/deepface_models/releases/download/v1.0/vgg_face_weights.h5'):
     model = baseModel()
     # -----------------------------------
 
@@ -67,5 +67,3 @@ def loadModel(url='https://github.com/serengil/deepface_models/releases/download
     # TO-DO: why?
     descriptor = Model(inputs=model.layers[0].input, outputs=model.layers[-2].output)
     return descriptor
-
-loadModel()
---
+++ FACE_CLASSIFICATION/train_v2.py
@@ -0,0 +1,83 @@
+# Our source library
+import numpy as np
+import cv2
+import os
+import sys
+import argparse
+from tqdm import tqdm
+
+# Standardize features
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.svm import SVC
+
+# My source library
+from utils.dataset import FaceRecognitionDataset
+from utils.general import LossCallback, save_modelckpt
+from backbone.network import build_model
+from backbone import FaceNet, VGGFace
+
+ALGORITHMS = ['svm', 'softmax']
+PRETRAIN = {'vggface': VGGFace.VGGFaceModel(),
+            'facenet': FaceNet.InceptionResNetV2()}
+
+
+# ArgumentParser
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', type=str, default='D:/Machine_Learning/face-recgnition/config/data.yaml')
+    parser.add_argument('--algorithms', type=str, default='svm')
+    parser.add_argument('--pretrain', type=str, default='vggface')
+    parser.add_argument('--save_model', type=bool, default=True)
+    args = parser.parse_args()
+    return args
+
+
+# Train model
+def run(args):
+    config, algorithms, pretrain, save = args.config, args.algorithms, args.pretrain, args.save_model
+
+    if algorithms.lower() in ALGORITHMS and pretrain.lower() in PRETRAIN:
+        # Load data
+        print("Loading dataset....")
+        Data = FaceRecognitionDataset(config=config)
+        data, label = Data.dataloader()
+        print("Loaded dataset...")
+
+        # Split dataset
+        Xtrain, Xtest, Ytrain, Ytest = Data.split_data(0.2, data, label)
+
+        # Load model pretrain
+        model = PRETRAIN.get(pretrain)
+
+        # Embedding vector train by input shape
+        embeddings_train = np.zeros((Xtrain.shape[0], 2622))
+        embeddings_test = np.zeros((Xtest.shape[0], 2622))
+        for i, img in enumerate(Xtrain):
+            embedding_vector = model.predict(np.expand_dims(img, axis=0))[0]
+            embeddings_train[i] = embedding_vector
+
+        # Standard data
+        scaler = StandardScaler()
+        Xtrain_std = scaler.fit_transform(embeddings_train)
+        Xtest_std = scaler.fit_transform(embeddings_test)
+
+        # Reduce dimensional
+        pca = PCA(n_components=128)
+        X_train_pca = pca.fit_transform(Xtrain_std)
+        X_test_pca = pca.transform(Xtest_std)
+
+        #  Train
+        clf = SVC(C=5., gamma=0.001)
+        clf.fit(X_train_pca, Ytrain)
+        y_predict = clf.predict(X_test_pca)
+
+
+
+    else:
+        raise TypeError
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    run(args)
